7e00b0dabbb37b6db0cc0df0bc39fc36d7b05bbc
Matthew Mannix <m.mannix3@gmail.com>
2016-12-05 09:57:19 -0500

    Add cities.txt and cities2.txt input files.

--- tsp.c
+++ tsp.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define NUM_CITIES 100
+#define NUM_CITIES 5
 
 #define DEBUG
 
@@ -26,9 +26,7 @@ typedef struct {
     double y;
 } City;
 
-typedef struct {
-    int cities[NUM_CITIES];
-} Path;
+//typedef int Path[NUM_CITIES];
 
 void city_init(City* city, double x, double y) {
     if(city != NULL) {
@@ -49,14 +47,14 @@ double city_calc_distance(City* a, City* b) {
     return sqrt( pow(b->x - a->x, 2) + pow(b->y - a->y, 2) );
 }
 
-void path_calc_fitness(Path* path, double* total_distance) {
+void path_calc_fitness(int path[NUM_CITIES], double* total_distance) {
     *total_distance = 0.0;
     for(int i = 1; i < NUM_CITIES; i++) {
         //Calculate the distance between each City in the Path
-        *total_distance += DISTANCE[path->cities[i-1]][path->cities[i]];
+        *total_distance += DISTANCE[path[i-1]][path[i]];
     }
     //Calculate the distance back to the home City
-    *total_distance += DISTANCE[path->cities[NUM_CITIES-1]][path->cities[0]];
+    *total_distance += DISTANCE[path[NUM_CITIES-1]][path[0]];
 }
 
 int main(int argc, char** argv) {
@@ -80,15 +78,31 @@ int main(int argc, char** argv) {
     for(int i = 0; i < NUM_CITIES; i++) { 
         fscanf(file, "%lf %lf", &tempX, &tempY); 
         city_init(&cities[i], tempX, tempY);
-        city_print(&cities[i]);
+        //city_print(&cities[i]);
     }
+    
+    DEBUG_PRINT("Cities loaded.\n");
     
     //Populate the DISTANCE lookup table
     for(int i = 0; i < NUM_CITIES; i++) {
         for(int j = 0; j < NUM_CITIES; j++) {
             DISTANCE[i][j] = city_calc_distance(&cities[i], &cities[j]); 
+            //DEBUG_PRINT("%d,%d:%.02f\n", i, j, DISTANCE[i][j]);
         }
     }
-
+    
+    DEBUG_PRINT("DISTANCE lookup table populated.\n");
+    
+    //Make a Path
+    //Path* path = malloc(sizeof(Path));
+    int path[NUM_CITIES];
+    for(int i = 0; i < NUM_CITIES; i++) {
+        path[i] = i;
+    }
+    
+    double distance;
+    path_calc_fitness(path, &distance);
+ 
+    printf("Distance of path 1: %.02f\n", distance); 
     return 0;
 }
